fc4ea5ed022b7c3c8c7e7102ffebbdc924f95a11
Precious Jatau <preciousjatau@ou.edu>
2021-12-15 13:43:53 -0600

    GetVADMaskDescription(). Gets description of a VADMask enum.

--- VADMaskEnum.py
+++ VADMaskEnum.py
@@ -1,7 +1,20 @@
 import enum
+
 class VADMask(enum.Enum):
     default = 0
     biological = 1
     insects = 2
     birds = 3
     weather = 4
+
+def GetVADMaskDescription(in_enum):
+    if in_enum == VADMask.default:
+        return "default"
+    elif in_enum == VADMask.biological:
+        return "biological"
+    elif in_enum == VADMask.insects:
+        return "insects"
+    elif in_enum == VADMask.birds:
+        return "birds"
+    elif in_enum == VADMask.weather:
+        return "weather"
