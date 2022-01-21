94e071d72780b43779c55112144486b648eb1202
Precious Jatau <preciousjatau@ou.edu>
2022-01-21 16:02:46 -0600

    Get plot title and output figure name from other inputs.

--- DebugSoundingData.py
+++ DebugSoundingData.py
@@ -28,8 +28,9 @@ def GetRadarScansNearSounding(batch_folder_path_radar, start_day, stop_day, date
 
 
 def Main():
-    radar_t_sounding = {'KHTX': 'BNA', 'KTLX': 'LMN', 'KOHX': 'BNA'}
-    station_infos = {'LMN': ('74646', 'Lamont, Oklahoma'), 'BNA': ('72327', 'Nashville, Tennessee')}
+    radar_t_sounding = {'KHTX': 'BNA', 'KTLX': 'LMN', 'KOHX': 'BNA', 'KENX': 'ALB'}
+    station_infos = {'LMN': ('74646', 'Lamont, Oklahoma'), 'BNA': ('72327', 'Nashville, Tennessee'),
+                     'ALB': ('72518', 'Albany, New York')}
 
     radar_data_folder = "./radar_data"
     hca_data_folder = "./level3_data"
@@ -46,85 +47,116 @@ def Main():
     if not os.path.isdir(figure_dir):
         os.makedirs(figure_dir)
 
-    start_day = 16
-    stop_day = 31
-    batch_folder_base = "KOHX_201805{}_201805{}"
+    nrows = 3
+    ncols = 4
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(6.4 * 2.2, 4.8 * 2.0))
 
-    start_day_str = '0' + str(start_day) if start_day < 10 else str(start_day)
-    stop_day_str = '0' + str(stop_day) if stop_day < 10 else str(stop_day)
-    batch_folder = batch_folder_base.format(start_day_str, stop_day_str)
+    start_days = [1]  # [1, 16]
+    stop_days = [31]  # [15, 31]
+    batch_folder_base = "KENX_201805{}_201805{}"  # "KOHX_201805{}_201805{}"
 
-    batch_folder_path_radar = os.path.join(radar_data_folder, batch_folder)
-    date_pattern = "*KOHX201805{}*_V06.*"
-    scans_near_sounding = GetRadarScansNearSounding(batch_folder_path_radar, start_day, stop_day, date_pattern)
+    title_base = "Soundings from {} {}. {}/{}/{} - {}/{}/{}"
+    # "Soundings from ALB Albany, New York. 05/01/2018 - 05/31/2018"
 
-    # Read radar data.
-    for radar_data_file in scans_near_sounding:
-        radar_data_file, _, data_table, radar_obj, hca_vol = PrepareAnalyzeWindInputs(radar_data_file,
-                                                                                      batch_folder,
-                                                                                      radar_data_folder,
-                                                                                      hca_data_folder,
-                                                                                      clf_file,
-                                                                                      is_batch)
+    min_start_day = "32"
+    max_stop_day = "00"
+    start_month = '12'
+    stop_month = '0'
+    start_year = '3000'
+    stop_year = '0'
+    counter = 0
 
-        if radar_obj is None:
-            continue
+    for job_id in range(len(start_days)):
 
-        # radar info
-        radar_name, year, month, day, hh, mm, ss = read_info_from_radar_name(radar_data_file)
-        radar_data_file_no_ext = os.path.splitext(radar_data_file)[0]
+        start_day = start_days[job_id]
+        stop_day = stop_days[job_id]
 
-        location_radar = {"latitude": radar_obj.latitude['data'][0],
-                          "longitude": radar_obj.longitude['data'][0],
-                          "height": radar_obj.altitude['data'][0]}
+        start_day_str = '0' + str(start_day) if start_day < 10 else str(start_day)
+        stop_day_str = '0' + str(stop_day) if stop_day < 10 else str(stop_day)
+        min_start_day = min(min_start_day, start_day_str)
+        max_stop_day = max(max_stop_day, stop_day_str)
 
-        station_id = station_infos[radar_t_sounding[radar_name]][0]
-        sounding_wind_df, sounding_location, sounding_url = GetSoundingWind(sounding_url_base, radar_data_file,
-                                                                            location_radar,
-                                                                            station_id, sounding_log_dir,
-                                                                            showDebugPlot=False, log_sounding_data=True,
-                                                                            force_website_download=True,
-                                                                            return_all_fields=True)
-        # print(sounding_wind_df.columns)
-        if sounding_wind_df is None:
-            continue
+        batch_folder = batch_folder_base.format(start_day_str, stop_day_str)
+        batch_folder_path_radar = os.path.join(radar_data_folder, batch_folder)
 
-        sound_year, sound_month, sound_ddhh_start, sound_ddhh_end, sound_station_id = parse.parse(sounding_url_base,
-                                                                                                  sounding_url)
+        date_pattern = "*KENX201805{}*_V06.*"  # "*KOHX201805{}*_V06.*"
+        scans_near_sounding = GetRadarScansNearSounding(batch_folder_path_radar, start_day, stop_day, date_pattern)
 
-        variables_plot = ['DRCT', 'SMPS', 'TEMP', 'PRES', 'DWPT', 'RELH', 'MIXR', 'THTA', 'THTE', 'THTV']
-        units_plot = [r'[$^{\circ}$]', '[m/s]', '[C]', '[hPa]', '[C]', '[%]', '[g/kg]', '[K]', '[K]', '[K]']
-        title_plot_base = "{}, {}/{}/{}, {}:{}:{} UTC\n" \
-                          "{}, {}/{}/{}, {}:00 UTC"
-        title_plot = title_plot_base.format(radar_name, month, day, year, hh, mm, ss,
-                                            radar_t_sounding[radar_name], sound_month, sound_ddhh_start[:2], sound_year,
-                                            sound_ddhh_start[2:])
+        # Read radar data.
+        for radar_data_file in scans_near_sounding:
+            radar_data_file, _, data_table, radar_obj, hca_vol = PrepareAnalyzeWindInputs(radar_data_file,
+                                                                                          batch_folder,
+                                                                                          radar_data_folder,
+                                                                                          hca_data_folder,
+                                                                                          clf_file,
+                                                                                          is_batch)
 
-        nrows = 3
-        ncols = 4
-        fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(6.4 * 2.2, 4.8 * 2.0))
-        for plot_idx in range(len(variables_plot)):
-            curr_var = variables_plot[plot_idx]
-            ax[plot_idx // ncols][plot_idx % ncols].scatter(sounding_wind_df[curr_var], sounding_wind_df['HGHT'],
-                                                            alpha=0.5)
-            ax[plot_idx // ncols][plot_idx % ncols].plot(sounding_wind_df[curr_var], sounding_wind_df['HGHT'])
-            ax[plot_idx // ncols][plot_idx % ncols].set_title(variables_plot[plot_idx])
-            ax[plot_idx // ncols][plot_idx % ncols].set_ylim(0, 1000)
-            ax[plot_idx // ncols][plot_idx % ncols].set_xlabel(
-                " ".join([variables_plot[plot_idx], units_plot[plot_idx]]))
-            ax[plot_idx // ncols][plot_idx % ncols].grid(True)
-        plt.tight_layout()
+            if radar_obj is None:
+                continue
 
-        fig.add_subplot(111, frameon=False)
-        plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
-        plt.ylabel('HGHT [m]')
-        fig.subplots_adjust(top=0.9)
-        plt.suptitle(title_plot)
+            # radar info
+            radar_name, year, month, day, hh, mm, ss = read_info_from_radar_name(radar_data_file)
+            start_month = min(start_month, month)
+            stop_month = max(stop_month, month)
+            start_year = min(start_year, year)
+            stop_year = max(stop_year, year)
 
-        if save_fig:
-            plt.savefig(os.path.join(figure_dir, radar_data_file_no_ext + '.png'), )
+            radar_data_file_no_ext = os.path.splitext(radar_data_file)[0]
 
-        # plt.show()
+            location_radar = {"latitude": radar_obj.latitude['data'][0],
+                              "longitude": radar_obj.longitude['data'][0],
+                              "height": radar_obj.altitude['data'][0]}
+
+            station_id = station_infos[radar_t_sounding[radar_name]][0]
+            sounding_wind_df, sounding_location, sounding_url = GetSoundingWind(sounding_url_base, radar_data_file,
+                                                                                location_radar,
+                                                                                station_id, sounding_log_dir,
+                                                                                showDebugPlot=False,
+                                                                                log_sounding_data=True,
+                                                                                force_website_download=False,
+                                                                                return_all_fields=True)
+            if sounding_wind_df is None:
+                continue
+
+            sound_year, sound_month, sound_ddhh_start, sound_ddhh_end, sound_station_id = parse.parse(sounding_url_base,
+                                                                                                      sounding_url)
+
+            variables_plot = ['DRCT', 'SMPS', 'TEMP', 'PRES', 'DWPT', 'RELH', 'MIXR', 'THTA', 'THTE', 'THTV']
+            units_plot = [r'[$^{\circ}$]', '[m/s]', '[C]', '[hPa]', '[C]', '[%]', '[g/kg]', '[K]', '[K]', '[K]']
+
+            for plot_idx in range(len(variables_plot)):
+                curr_var = variables_plot[plot_idx]
+                ax[plot_idx // ncols][plot_idx % ncols].plot(sounding_wind_df[curr_var], sounding_wind_df['HGHT'],
+                                                             alpha=0.6)
+                ax[plot_idx // ncols][plot_idx % ncols].set_title(variables_plot[plot_idx])
+                ax[plot_idx // ncols][plot_idx % ncols].set_ylim(-400, 1000)
+                ax[plot_idx // ncols][plot_idx % ncols].set_xlabel(
+                    " ".join([variables_plot[plot_idx], units_plot[plot_idx]]))
+                ax[plot_idx // ncols][plot_idx % ncols].grid(True)
+            # plt.show()
+
+            counter += 1
+            if counter == 1:
+                break
+
+    plt.tight_layout()
+
+    title = title_base.format(radar_t_sounding[radar_name], station_infos[radar_t_sounding[radar_name]][1],
+                              start_month, min_start_day, start_year, stop_month, max_stop_day, stop_year)
+    out_name = "{}{}{}_{}{}{}_{}.png".format(start_month, min_start_day, start_year, stop_month, max_stop_day,
+                                             stop_year, radar_t_sounding[radar_name])
+
+    fig.add_subplot(111, frameon=False)
+    plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
+    plt.ylabel('HGHT [m]')
+    fig.subplots_adjust(top=0.9)
+    plt.suptitle(title)
+    # plt.show()
+
+    if save_fig:
+        plt.savefig(os.path.join(figure_dir, out_name))
+
+    plt.show()
 
 
 Main()
