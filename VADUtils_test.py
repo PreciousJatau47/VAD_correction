e481fc1cc5f6fb51551f650e8970ce833126749c
Precious Jatau <preciousjatau@ou.edu>
2023-03-10 14:36:06 -0600

    Added tests for fitVAD() and mixVADs()

--- VADUtils_test.py
+++ VADUtils_test.py
@@ -47,3 +47,77 @@ class TestVADUtils(unittest.TestCase):
         pred_ins_mask = GetVADMask(data_table, VADMask.insects, tail_threshold)
         print("pred. insects: ", np.sum(pred_ins_mask))
         assert np.logical_and.reduce(exp_ins_mask == pred_ins_mask)
+
+    def test_MixVADs(self):
+        N = 720 * 2
+        t = np.linspace(0, 360, N)
+
+        # Generate VADs
+        true_wind_speed = 15
+        true_wind_dir = 90
+        vad_main = GenerateVAD(speed=true_wind_speed, dirn=true_wind_dir, dirn_grid=t) + GenerateNoiseNormal(
+            num_samples=N)
+        vad_sec = GenerateVAD(speed=11, dirn=68, dirn_grid=t) + GenerateNoiseNormal(num_samples=N)
+
+        # MixVADs
+        vad_mix, is_main_vad, a_idxs, mix_idxs, b_idxs, weights_mix = MixVADs(vad_main=vad_main, vad_sec=vad_sec,
+                                                                              a=1 / 20, mix_r=9 / 10)
+
+        self.assertTrue(np.array_equal(vad_mix[a_idxs], vad_main[a_idxs]))
+        self.assertTrue(np.array_equal(vad_mix[b_idxs], vad_sec[b_idxs]))
+        exp_mix = vad_mix[mix_idxs]
+        act_mix = weights_mix * vad_main[mix_idxs] + (1 - weights_mix) * vad_sec[mix_idxs]
+        self.assertTrue(np.array_equal(exp_mix, act_mix))
+
+        self.assertTrue(np.array_equal(is_main_vad[mix_idxs], weights_mix))
+        self.assertTrue(np.logical_and.reduce(is_main_vad[a_idxs] == 1))
+        self.assertTrue(np.logical_and.reduce(is_main_vad[b_idxs] == 0))
+
+        return
+
+    def test_fitVAD(self):
+        N = 720 * 2
+        t = np.linspace(0, 360, N)
+
+        # Unweighted VAD
+        true_wind_speed = 11
+        true_wind_dir = 60
+        data = GenerateVAD(speed=true_wind_speed, dirn=true_wind_dir, dirn_grid=t) + GenerateNoiseNormal(num_samples=N)
+        wind_speed, wind_dir, vad_fit = fitVAD(t, data, signal_func, showDebugPlot=False, description='')
+        self.assertTrue(np.isclose(a=true_wind_speed, b=wind_speed, rtol=1, atol=1))
+        self.assertTrue(np.isclose(a=true_wind_dir, b=wind_dir, rtol=5, atol=5))
+
+        # Weighted VAD
+        true_wind_speed = 15
+        true_wind_dir = 90
+        vad_main = GenerateVAD(speed=true_wind_speed, dirn=true_wind_dir, dirn_grid=t) + GenerateNoiseNormal(
+            num_samples=N)
+        vad_sec = GenerateVAD(speed=11, dirn=68, dirn_grid=t) + GenerateNoiseNormal(num_samples=N)
+
+
+        vad_mix, is_main_vad, a_idxs, mix_idxs, b_idxs, weights_mix = MixVADs(vad_main=vad_main, vad_sec=vad_sec,
+                                                                              a=1 / 20, mix_r=9 / 10)
+
+        wind_speed, wind_dir, vad_fit = fitVAD(t, vad_main, signal_func, showDebugPlot=False, description='')
+        print()
+        print("Fit on pure VAD")
+        print(wind_speed)
+        print(wind_dir)
+        print()
+
+        wind_speed, wind_dir, vad_fit = fitVAD(t, vad_mix, signal_func, showDebugPlot=False, description='')
+        print("Weighted fit on contaminated VAD")
+        print(wind_speed)
+        print(wind_dir)
+        print()
+
+        wind_speed, wind_dir, vad_fit = fitVAD(t, vad_mix, signal_func, showDebugPlot=False, description='',
+                                               weights=is_main_vad)
+        print("Unweighted fit on contaminated VAD")
+        print(wind_speed)
+        print(wind_dir)
+        print()
+
+
+if __name__ == "__main__":
+    unittest.main()
